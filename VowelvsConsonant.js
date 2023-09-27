a5b98b082827115d85d18bac6b32995ffc4ab854
Robert Allen <robertallen@Roberts-MBP.attlocal.net>
2023-09-26 20:48:27 -0500

    first commit

---
+++ VowelvsConsonant.js
@@ -0,0 +1,64 @@
+// create code that will use lowercase letters only.
+// The output to the console.log should include the string being searched
+//  and how many consenants and vowels are within the set
+
+// const string = "hello"
+
+// const count = string => {
+
+// let vowel = 0;
+// let consonant = 0;
+
+// for (let i = 0; i <string.length; i++) {
+//     let element = string[i];
+
+//   if (ele == 'a' || ele == 'e' || ele == 'i' || ele == 'o' || ele == 'u')
+
+//   vow += 1;
+//   else
+//   cons += 1;
+
+//   };
+
+//   console.log(`${string}`);
+//   console.log(`${vow} vowels present`);
+//   console.log(`${cons} consonants are present.`);
+
+// };
+
+// const string = "hello";
+
+// let vowels = ["a", "e", "i", "o", "u"];
+// let consonants = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z"];
+
+// for (let i = 0; i < string.length; i++) {
+//     if (string[i] === "a", "e", "i", "o", "u") {
+//         count += 1;
+
+//     if (string[i] === "b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z")
+//         count += 1;
+//     }
+// }
+
+// console.log(string, count);
+
+// Instruction from Byron
+
+const testString = "hellow has 3 consonants and 2 vowels";
+
+const letters = "ae";
+
+Let aeLetters = 0;
+let others = 0;
+
+for(let i = 0; i < testString.length; i++){
+
+    if(letters.indexOf(testString[i].toLowerCase()) !== -1) {
+        aeLetters += 1;
+    } else if (testString[i] !== " ") {
+        others += 1;
+    }
+}
+
+console.log(aeLetters);
+console.log(others);
